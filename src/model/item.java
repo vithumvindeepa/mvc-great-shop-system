73043c16cc3f29f5dd9eeca3fbebcc8570c20c82
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-02 22:31:43 +0530

    creat item get set methods

--- src/model/item.java
+++ src/model/item.java
@@ -12,11 +12,59 @@ public class item {
     }
 
     public item(int itemCode, String description, int packSize, int quantityOnHand, int unitPrice, int itemType) {
+        this.setItemCode(itemCode);
+        this.setDescription(description);
+        this.setPackSize(packSize);
+        this.setQuantityOnHand(quantityOnHand);
+        this.setUnitPrice(unitPrice);
+        this.setItemType(itemType);
+    }
+
+    public int getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(int itemCode) {
         this.itemCode = itemCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getPackSize() {
+        return packSize;
+    }
+
+    public void setPackSize(int packSize) {
         this.packSize = packSize;
+    }
+
+    public int getQuantityOnHand() {
+        return quantityOnHand;
+    }
+
+    public void setQuantityOnHand(int quantityOnHand) {
         this.quantityOnHand = quantityOnHand;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
         this.unitPrice = unitPrice;
+    }
+
+    public int getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(int itemType) {
         this.itemType = itemType;
     }
 }
