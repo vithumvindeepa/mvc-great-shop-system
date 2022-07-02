8d7c6314c8f43a212e2d5c467fb5f2efe18dfee1
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-02 22:24:51 +0530

    creat get set methods

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -13,12 +13,68 @@ public class Customer {
     }
 
     public Customer(int customerID, String customerTitle, String name, String address, String city, String province, int postalCode) {
+        this.setCustomerID(customerID);
+        this.setCustomerTitle(customerTitle);
+        this.setName(name);
+        this.setAddress(address);
+        this.setCity(city);
+        this.setProvince(province);
+        this.setPostalCode(postalCode);
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
         this.customerID = customerID;
+    }
+
+    public String getCustomerTitle() {
+        return customerTitle;
+    }
+
+    public void setCustomerTitle(String customerTitle) {
         this.customerTitle = customerTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
         this.province = province;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
         this.postalCode = postalCode;
     }
 }
