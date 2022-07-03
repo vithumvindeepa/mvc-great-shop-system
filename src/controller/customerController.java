1964178eed4287aa578eecb29dd8fdbc079ec126
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-03 22:50:23 +0530

    update

--- src/controller/customerController.java
+++ src/controller/customerController.java
@@ -1,14 +1,31 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class customerController {
     public AnchorPane customerContext;
-    public JFXTextField customer;
     public JFXTextField txtcustomerId;
+    public TableView tblCustomer;
+    public TableColumn colId;
+    public TableColumn colTital;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colCity;
+    public TableColumn colProvide;
+    public TableColumn colPostalCode;
+    public JFXTextField txtTitle;
+    public JFXTextField txtPostalCode;
+    public JFXTextField txtName;
+    public JFXTextField txtProvince;
+    public JFXTextField txtAddreaa;
+    public JFXTextField txtCity;
+    public JFXButton btnSave;
 
     public void btnSave(ActionEvent actionEvent) {
 
